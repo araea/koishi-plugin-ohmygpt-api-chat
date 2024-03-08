8f0d09804514ba9b146d43d53759efe278d0f2fb
Nawyjx <3373167460@qq.com>
2024-03-09 01:36:39 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -704,7 +704,7 @@ export function apply(ctx: Context, config: Config) {
       const imageBuffer = await ctx.markdownToImage.convertToImage(modifiedMessage);
       return await sendMessage(session, `${h.image(imageBuffer, 'image/png')}`)
     } else {
-      return await sendMessage(session, `${message}`)
+      return await sendMessage(session, message)
     }
   }
 
